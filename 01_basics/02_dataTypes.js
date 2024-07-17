da826b4aedcf769956977adcdc853e491ba80653
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2024-07-17 12:35:59 +0000

    datatypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";  //treats all js code as newer version.
+
+// alert(3+3);    //we are using nodejs and not in browser thus we can not use alert like this.
+
+
+console.log("Jai shree Ram");
+
+let name = "Dev Poonia";
+let age = 21;
+let isMarried = false;
+let kids = null;
+let status;
+
+
+console.table([name, age, isMarried, kids, status]);
+console.log(typeof "false");
