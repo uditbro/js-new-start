a97a932e6736d3acb137a1735e1b75d3fba43a76
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2024-07-17 12:18:41 +0000

    variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 21122
+let accountEmail = "dev@gmail.com"
+let password = "12453"
+let accountState
+
+/*
+only use let and const for variable decleration and don not use var at all.
+*/ 
+
+console.table([accountId, accountEmail, password, accountState]);
+/*
+console.table to declare all the variable in the tabular form on the screen.
+*/
