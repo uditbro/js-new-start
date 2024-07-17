77d8fef82be626d7a766eeee18676304a766600b
Dev Poonia <110079684+uditbro@users.noreply.github.com>
2024-07-17 13:00:40 +0000

    datatype conversion confusion (imp. for backend)

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -12,4 +12,7 @@ console.log(valueInNumber);
 // "33" => 33 by using Number("33");
 // "33abc" => NaN by using Number("33abc");
 // null => 0 by using number(null);
+// true =>1 or "abc11", false => 0 or "".
 //so be sure to check out the number while working on the backend cause any form of data can arrive from the frontend part.
+//strings also change the same way number did.
+
